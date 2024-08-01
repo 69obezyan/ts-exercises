98d787fd9fc3a7fd420b113e7af91ce268086f3f
69obezyan <tvoyamama94@gmail.com>
2024-08-01 14:38:07 +0300

    init

---
+++ exercises/00-warm-up/src/01-array-to-string/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Write a function that converts the given array of words into a sentence.
+ * All words in the sentence should be separated by a comma and a space.
+ * Return the result.
+ * 
+ * Example
+ * 
+ * Input: ["Banana", "apple"]
+ * Output: "Banana, apple"
+ */
+
+function arrayToString(input: string[]) {
+}
+
+export { arrayToString };
