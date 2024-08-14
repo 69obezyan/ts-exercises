637c771f79d9637c626c51c98a8784997bad5e84
69obezyan <tvoyamama94@gmail.com>
2024-08-14 16:27:02 +0300

    ch 03 compl

--- exercises/01-challenges/src/03-secret-society/index.ts
+++ exercises/01-challenges/src/03-secret-society/index.ts
@@ -4,6 +4,11 @@
  * Create a function that takes in an array of names and returns the name of the secret society.
  */
 
-function secretSociety(names: string[]) { }
+function secretSociety(names: string[]) {
+    const firstLetters = names.map(name => name[0]);
+    firstLetters.sort();
+    const societyName = firstLetters.join('');
+    return societyName;
+ }
 
 export { secretSociety };
