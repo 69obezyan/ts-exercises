0320ca226e7ca9b60416361434bfd40386b14a26
69obezyan <tvoyamama94@gmail.com>
2024-08-14 16:15:53 +0300

    ch 02 compl

--- exercises/01-challenges/src/02-hide-and-seek/index.ts
+++ exercises/01-challenges/src/02-hide-and-seek/index.ts
@@ -3,6 +3,14 @@
  * Write a function that returns the hidden name.
  */
 
-function hideAndSeek(crowd: string) { }
+function hideAndSeek(crowd: string): string {
+    let hiddenName = '';
+    for (let i = 0; i < crowd.length; i++) {
+        if (crowd[i] >= 'a' && crowd[i] <= 'z') {
+            hiddenName += crowd[i];
+        }
+    }
+    return hiddenName;
+}
 
 export { hideAndSeek };
