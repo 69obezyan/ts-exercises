80d11fcfac52b378ebd22f04922a42ae7e276a2e
69obezyan <tvoyamama94@gmail.com>
2024-08-14 16:43:59 +0300

    ch 04 com

--- exercises/01-challenges/src/04-discount/index.ts
+++ exercises/01-challenges/src/04-discount/index.ts
@@ -3,6 +3,8 @@
  * Return the final price after the discount.
  */
 
-function discount(price: number, discount: number) { }
+function discount(price: number, discount: number) { 
+    return (price - (price * discount) / 100);
+}
 
 export { discount };
