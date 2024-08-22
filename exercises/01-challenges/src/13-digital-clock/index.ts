206e3b20a1416e6b7f3bcc7c1358c9dd154cea7f
69obezyan <tvoyamama94@gmail.com>
2024-08-22 15:03:06 +0300

    ch 13 compl

--- exercises/01-challenges/src/13-digital-clock/index.ts
+++ exercises/01-challenges/src/13-digital-clock/index.ts
@@ -3,6 +3,16 @@
  * Time should be counted from 00:00:00.
  */
 
-function digitalClock(seconds: number) { }
+function digitalClock(seconds: number): string {
+    const totalHours = Math.floor(seconds / 3600);
+    const hours = totalHours % 24; // Ensure hours do not exceed 24
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
 
-export { digitalClock };
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  }
+  
+  export { digitalClock };
