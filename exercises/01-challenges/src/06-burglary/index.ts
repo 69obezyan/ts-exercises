d3ff1437be60f670ae3bd758cfa73052e711f803
69obezyan <tvoyamama94@gmail.com>
2024-08-22 13:54:26 +0300

    06 ch compl

--- exercises/01-challenges/src/06-burglary/index.ts
+++ exercises/01-challenges/src/06-burglary/index.ts
@@ -4,6 +4,19 @@
  * If nothing was robbed, return the string "Lucky you!".
  */
 
-function burglary(input: any) { }
+function burglary(input: any) {
+    if (Object.keys(input).length === 0) {
+        return "Lucky you!";
+    }
+
+    // Calculate the total value of the stolen items
+    let totalValue = 0;
+    for (let item in input) {
+        if (input.hasOwnProperty(item)) {
+            totalValue += input[item];
+        }
+    }
+    return totalValue;
+ }
 
 export { burglary };
