cf8d22e2a8ae26712c1472a388899bd168f47c24
69obezyan <tvoyamama94@gmail.com>
2024-08-22 14:39:42 +0300

    ch 11 com

--- exercises/01-challenges/src/11-kindergarten/index.ts
+++ exercises/01-challenges/src/11-kindergarten/index.ts
@@ -3,6 +3,18 @@
  * Write a function that reverses all the words in a sentence that start with a particular letter.
  */
 
-function kindergarten(sentence: string, letter: string) { }
+function kindergarten(sentence: string, letter: string) {
+    let words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        // Check if the word starts with the given letter (case-insensitive)
+        if (word.charAt(0).toLowerCase() === letter.toLowerCase()) {
+            // Reverse the word
+            words[i] = word.split('').reverse().join('');
+        }
+    }
+    return words.join(' ');
+ }
 
 export { kindergarten };
