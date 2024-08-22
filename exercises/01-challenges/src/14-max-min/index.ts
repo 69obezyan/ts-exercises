b135add0df5b84651cd9e21ea01e041815c6c0f0
69obezyan <tvoyamama94@gmail.com>
2024-08-22 15:04:06 +0300

    ch 14 compl

--- exercises/01-challenges/src/14-max-min/index.ts
+++ exercises/01-challenges/src/14-max-min/index.ts
@@ -2,6 +2,15 @@
  * Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order in a new array.
  */
 
-function maxMin(numbers: number[]) { }
-
-export { maxMin };
+function maxMin(numbers: number[]): number[] {
+    if (numbers.length === 0) {
+      throw new Error("Array must contain at least one number");
+    }
+  
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+  
+    return [min, max];
+  }
+  
+  export { maxMin };
